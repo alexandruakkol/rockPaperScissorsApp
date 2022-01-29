7f437866e5603eefb2039e7a7d0018eb28793b2c
Alexandru <alexandru.akkol@gmail.com>
2022-01-29 15:23:57 +0200

    rockpaperscissors

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-function getInput(){
-    choice = prompt("Choose: rock/paper/scissors");
-    return choice;
+var playerScore = 0;
+var pcScore = 0;
+var GAME_OVER = false;
+
+function getComputerChoice(){
+    random = Math.random();
+    
+    if(random<1/3)return "rock";
+    if(random>=1/3 && random <2/3)return "paper";
+    if(random>=2/3)return "scissors";
 }
-let r = Math.random();
-let compC = null;
-let msg = "tie";
 
-if(r<0.33){compC = "rock"}
-if(r>=0.33 && r<0.66){compC = "paper"}
-if(r>=0.66){compC = "scissors"}
+function playRound(playerChoice){
+    if(GAME_OVER){return;}
+    pcChoice = getComputerChoice();
+    console.log(`computer chose: ${pcChoice}, player chose: ${playerChoice}.`);
+    h4 = document.querySelector('.response');
+    outcomeElement = document.querySelector(".outcome");
+    playerScoreElement = document.querySelector(".Pscore");
+    pcScoreElement = document.querySelector(".Cscore");
+    bodyElement = document.querySelector("body");
+    if(pcChoice === playerChoice){h4.textContent="Tie. Computer chose "+pcChoice+".";}
+    if(playerChoice === "rock" && pcChoice === "paper" ){h4.textContent="You lose this round. Computer chose "+pcChoice+".";pcScore++;}
+    if(playerChoice === "rock" && pcChoice === "scissors"){h4.textContent="You win this round. Computer chose "+pcChoice+".";playerScore++;}
+    if(playerChoice === "paper" && pcChoice === "scissors" ){h4.textContent="You lose this round. Computer chose "+pcChoice+".";pcScore++;}
+    if(playerChoice === "paper" && pcChoice === "rock"){h4.textContent="You win this round. Computer chose "+pcChoice+".";playerScore++;}
+    if(playerChoice === "scissors" && pcChoice === "rock" ){h4.textContent="You lose this round. Computer chose "+pcChoice+".";pcScore++;}
+    if(playerChoice === "scissors" && pcChoice === "paper"){h4.textContent="You win this round. Computer chose "+pcChoice+".";playerScore++;}
+    playerScoreElement.textContent = playerScore;
+    pcScoreElement.textContent = pcScore;
+    if(playerScore === 5){outcomeElement.textContent = "You Won !";bodyElement.style.backgroundColor = "green";GAME_OVER=true;}
+    if(pcScore === 5){outcomeElement.textContent = "You Lost !";bodyElement.style.backgroundColor = "red";GAME_OVER=true;}
 
-userC = getInput().toLowerCase();
+}
 
-if(userC==="rock" && compC === "scissors")msg="user wins"
-if(userC==="rock" && compC === "paper")msg="computer wins"
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
-if(userC==="paper" && compC === "rock")msg="user wins"
-if(userC==="paper" && compC === "scissors")msg="computer wins"
+rockBtn.addEventListener('click', function (){playRound('rock');});
+paperBtn.addEventListener('click', function (){playRound('paper');});
+scissorsBtn.addEventListener('click', function (){playRound('scissors');});
+rockBtn.playerChoice = 'rock';
 
-if(userC==="scissors" && compC === "rock")msg="computer wins"
-if(userC==="scissors" && compC === "paper")msg="user wins"
-
-alert("The computer chose "+ compC+ ", "+ msg+".")
